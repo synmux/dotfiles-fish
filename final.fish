662a77056d2b5c3e40a5f14ec65ca5e1391815d1
syn <syn@syn.as>
2026-09-15 18:57:04 +0100

    🔥 chore(fish): remove gtag alias for tag function
    - Drop the block in final.fish that copied the tag function to gtag
      and erased the original tag at startup
    - Delete the completions/gtag.fish symlink to tag.fish
    
    The rename was a workaround for a name clash that is no longer
    needed. tag now keeps its original name and completions, and gtag
    no longer exists.

--- final.fish
+++ final.fish
@@ -21,9 +21,3 @@ fish_add_path $HOME/.lmstudio/bin
 fish_add_path $HOME/.codeium/windsurf/bin
 fish_add_path $HOME/.opencode/bin
 fish_add_path $HOME/.miyo/bin
-
-# a bit of function meddling
-if functions --query tag
-    functions --copy tag gtag
-    functions --erase tag
-end
